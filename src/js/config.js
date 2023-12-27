20e3d45b73b47dfcc91d38ef3861c643d5cc383b
RKolar <bato9440@hotmail.com>
2023-12-27 13:46:04 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `f54a0cc1-971c-42e7-860f-3c9e756bae99`;
+export const MODAL_CLOSE_SEC = 2.5;
